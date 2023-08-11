9e9344cdb97939820ee6d97601796a6e1325f8ae
Arikabz <arturo.guza1999@gmail.com>
2023-08-10 22:22:20 -0600

    added alt property to img

--- packages/nextjs/components/BriefSubscriptionCard.tsx
+++ packages/nextjs/components/BriefSubscriptionCard.tsx
@@ -5,7 +5,10 @@ const BriefSubscriptionCard = () => {
     <div className="bg-base-100 border-base-300 border shadow-md shadow-secondary rounded-3xl mt-4 mx-4 flex h-[10vh]">
       <div className="avatar ">
         <div className="h-24 my-auto mx-4 rounded-xl">
-          <img src="https://images.ctfassets.net/3vz37y2qhojh/5ovxftNE5jbkvWUqcnohYk/472f2dbf2ec1eb26b1e9a5b879dca535/Korean_Burgers.jpg?w=750&fit=fill&fm=webp" />
+          <img
+            alt="Business"
+            src="https://images.ctfassets.net/3vz37y2qhojh/5ovxftNE5jbkvWUqcnohYk/472f2dbf2ec1eb26b1e9a5b879dca535/Korean_Burgers.jpg?w=750&fit=fill&fm=webp"
+          />
         </div>
       </div>
       <div className="card-body basis-1/2">
